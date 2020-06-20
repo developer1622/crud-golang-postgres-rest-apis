340ac9b6553253452e90fd06fcbd0c859b03bbfd
RAMU MANGALARAPU <ramu.mangalarapu1732@gmail.com>
2020-06-20 14:47:09 +0530

    Add Go Modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module crud-golang-postgres-rest-apis
+
+go 1.14
+
+require github.com/lib/pq v1.7.0
---
+++ server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	DB_USER     = "postgres"
+	DB_PASSWORD = "Postgres"
+	DB_NAME     = "todo_db"
+)
+
+func main() {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", dbinfo)
+	checkErr(err)
+	defer db.Close()
+
+	fmt.Println("# Inserting values")
+
+	sqlStatement := `INSERT INTO task VALUES(155452,'dffdf44444d','as44taxie');`
+	_, err = db.Exec(sqlStatement)
+	checkErr(err)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
